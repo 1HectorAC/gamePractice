2b1121319b72fc02d1b79eebd4d95558c56f7442
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2020-06-09 14:43:29 -0700

    Added comments to health.cpp.

--- health.cpp
+++ health.cpp
@@ -5,21 +5,25 @@ Health::Health(QGraphicsItem *parent)
     : QGraphicsTextItem(parent){
     health = 10;
 
-    // draw the text
-    setPlainText(QString("Health: ") + QString::number(health)); // Health: 3
+    // Draw text of health.
+    setPlainText(QString("Health: ") + QString::number(health));
     setDefaultTextColor(Qt::red);
     setFont(QFont("times",16));
 }
 
+// Decrease health by 1 and update text of health.
 void Health::decrease(){
     health--;
     setPlainText(QString("Health: ") + QString::number(health)); // Health: 2
 }
+
+// Increase health by 1 and update text of health.
 void Health::increase(){
     health+=1;
     setPlainText(QString("Health: ") + QString::number(health)); // Health: 2
 }
 
+// Get health.
 int Health::getHealth(){
     return health;
 }
