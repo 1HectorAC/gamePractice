5d09a6737c5ece947dc9ed831e2839101edd980a
Hector Acosta-Cervantes <hacostacervantes17@mail.wou.edu>
2020-06-10 15:09:28 -0700

    Added comments to score.cpp.

--- score.cpp
+++ score.cpp
@@ -1,19 +1,23 @@
 #include "Score.h"
-#include <QFont>
 #include "Game.h"
+#include <QFont>
+
 extern Game * game;
 Score::Score(QGraphicsItem *parent): QGraphicsTextItem(parent){
-    // initialize the score to 0
+    // Initialize the score to 0.
     score = 0;
 
-    // draw the text
+    // Draw the score text.
     setPlainText(QString("Score: ") + QString::number(score)); // Score: 0
     setDefaultTextColor(Qt::red);
     setFont(QFont("times",16));
 }
 
+// Increase Score by one.
 void Score::increase(){
     score++;
+
+    // Increase limiter value after a cetain amount of points earned in score.
     if (score % 30 == 1){
         game->limits->increase();
     }
@@ -21,8 +25,11 @@ void Score::increase(){
     setPlainText(QString("Score: ") + QString::number(score)); // Score: 1
 }
 
+// Increase Score by three.
 void Score::increase2(){
     score+=3;
+
+    // Increase limiter value after a cetain amount of points earned in score.
     if (score % 10 == 1){
         game->limits->increase();
     }
@@ -30,6 +37,7 @@ void Score::increase2(){
     setPlainText(QString("Score: ") + QString::number(score)); // Score: 1
 }
 
+// Get Score.
 int Score::getScore(){
     return score;
 }
